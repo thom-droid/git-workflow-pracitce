b693eff8419639cdcc823a06576b0efb56f3b328
thom-window <hsbang.thom@gmail.com>
2022-10-03 00:40:28 +0900

    menu

--- src/main/java/store/Store.java
+++ src/main/java/store/Store.java
@@ -1,7 +1,14 @@
 package store;
 
+import chicken.Chicken;
+
+import java.util.List;
+
 public class Store {
 
     private String name;
     private Address address;
+
+    private List<Chicken> menu;
+
 }

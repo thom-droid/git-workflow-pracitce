dd91985be606a40588865dd84fea61c859013fa7
thom-window <hsbang.thom@gmail.com>
2022-10-03 00:46:28 +0900

    pizza

--- src/main/java/chicken/Chicken.java
+++ src/main/java/chicken/Chicken.java
@@ -1,10 +1,22 @@
 package chicken;
 
+import java.util.List;
+
 public class Chicken {
 
     private String name;
     private Ingredients ingredients;
     private int price;
+
+    private Sauce sauce;
+
+    public Sauce getSauce() {
+        return sauce;
+    }
+
+    public void setSauce(Sauce sauce) {
+        this.sauce = sauce;
+    }
 
     public String getName() {
         return name;

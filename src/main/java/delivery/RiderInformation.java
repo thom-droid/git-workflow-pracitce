a5af4fa58c4fded1b2095d4af802330801dca3f8
thom-window <hsbang.thom@gmail.com>
2022-10-03 01:08:02 +0900

    rider info

---
+++ src/main/java/delivery/RiderInformation.java
@@ -0,0 +1,23 @@
+package delivery;
+
+public class RiderInformation {
+
+    private String name;
+    private Area area;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Area getArea() {
+        return area;
+    }
+
+    public void setArea(Area area) {
+        this.area = area;
+    }
+}

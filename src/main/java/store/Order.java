aba65c063847090a168e92e86059e19c00044706
thom-window <hsbang.thom@gmail.com>
2022-10-03 00:40:28 +0900

    menu

---
+++ src/main/java/store/Order.java
@@ -0,0 +1,14 @@
+package store;
+
+import chicken.Chicken;
+
+import java.util.List;
+
+public class Order {
+
+    private int number;
+    private int price;
+
+    private List<Chicken> chickens;
+
+}
